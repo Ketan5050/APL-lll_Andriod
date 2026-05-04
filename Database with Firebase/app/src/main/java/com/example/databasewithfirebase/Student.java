9550c5d8797a4db7f3f4fd2cbf11c1c14b32d40b
Ketan5050 <patilketya100@gmail.com>
2026-05-04 09:58:49 +0530

    updated

---
+++ Database with Firebase/app/src/main/java/com/example/databasewithfirebase/Student.java
@@ -0,0 +1,31 @@
+package com.example.databasewithfirebase;
+
+public class Student {
+    private String rollNo;
+    private String name;
+
+    public Student() {
+        // Default constructor required for calls to DataSnapshot.getValue(Student.class)
+    }
+
+    public Student(String rollNo, String name) {
+        this.rollNo = rollNo;
+        this.name = name;
+    }
+
+    public String getRollNo() {
+        return rollNo;
+    }
+
+    public void setRollNo(String rollNo) {
+        this.rollNo = rollNo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
